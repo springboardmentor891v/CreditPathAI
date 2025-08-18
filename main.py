73f0f7d330849ff70f0de9e1a2728204f6195e87
SpringBoard Mentor <springboardmentor891v@gmail.com>
2025-08-18 17:24:41 +0530

    add main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
