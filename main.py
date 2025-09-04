0441ba8bc83bfdd2559c16b2fb17fa93fd537d24
Rajath Raj <rajath032005@gmail.com>
2025-09-04 15:18:34 +0530

    ....

--- main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
